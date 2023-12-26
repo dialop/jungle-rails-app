e46dc3e0b9d05c69294553fd91eae841fe8574ae
dialop <dianalop1998>
2023-12-26 14:53:44 -0500

    Add admin category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,27 @@
 class Admin::CategoriesController < ApplicationController
-  # Existing show action
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+
+  def index
+    @categories = Category.all
   end
-end  
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+  
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
