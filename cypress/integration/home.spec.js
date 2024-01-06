6286f8606a2268206d1d4399208020dc92730ee9
dialop <dianalop1998@gmail.com>
2024-01-06 16:26:23 -0500

    Add Feature Spec: Home Page Pass

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,5 @@
 
-it("There are 2 products on the page", () => {
+it("There are 12 products on the page", () => {
   cy.visit("http://127.0.0.1:3000/");
-  cy.get(".products article").should("have.length", 2);
+  cy.get(".products article").should("have.length", 12);
 });
